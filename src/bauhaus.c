a85de74d103789837ef0637acc5bd528d2eefede
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2019-11-01 13:50:02 -0300

    Use default red as color

--- src/bauhaus.c
+++ src/bauhaus.c
@@ -8,7 +8,8 @@
 /* Function declarations ---------------------------------------------------- */
 
 static void print_block(const block_t);
-static void set_bg_color(const COLOR_T);
+/* static void set_bg_color(const COLOR_T); */
+static void reset_terminal_color();
 
 #define LENGHT(X) (sizeof X / sizeof X[0])
 
@@ -31,17 +32,22 @@ static void print_block(const block_t block) {
     char* msg = block.func(block.arg);
     if ( msg ) {
 
-        set_bg_color(color_bg);
+        /* set_bg_color(color_bg); */
+        printf("\\[\\e[41m\\]");
         printf(" %s ", msg);
-        set_bg_color(color_bg_reset);
+        reset_terminal_color();
 
     }
 
 }
 
-static void set_bg_color(const COLOR_T c) {
-    uint8_t r = ( c >> 16 ) & 0xFF;
-    uint8_t g = ( c >> 8 ) & 0xFF;
-    uint8_t b = c & 0xFF;
-    printf("\\[\\e[48;2;%d;%d;%dm\\]", r, g, b);
+// static void set_bg_color(const COLOR_T c) {
+//     uint8_t r = ( c >> 16 ) & 0xFF;
+//     uint8_t g = ( c >> 8 ) & 0xFF;
+//     uint8_t b = c & 0xFF;
+//     printf("\\[\\e[48;2;%d;%d;%dm\\]", r, g, b);
+// }
+
+static void reset_terminal_color() {
+    printf("\\[\\e[0m\\]");
 }
